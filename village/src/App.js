05bf5b72526456216127dfd038febe3fe2cd612b
Cesare Ferrari <cesare@maryandferrari.com>
2019-06-15 13:48:25 -0400

    Redirect to home page after adding Smurf

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route, NavLink } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -28,7 +29,8 @@ class App extends Component {
     axios
       .post('http://localhost:3333/smurfs', smurf)
       .then(res => {
-        this.setState({smurfs: res.data})
+        this.setState({smurfs: res.data});
+        this.props.history.push('/');
       })
       .catch(err => console.log(err));
   }
@@ -36,8 +38,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm addSmurf={this.addSmurf} />
-        <Smurfs smurfs={this.state.smurfs} />
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/smurf-form">Add Smurf</NavLink>
+      </nav>
+
+        <Route exact path="/" render={props => <Smurfs {...props} smurfs={this.state.smurfs} />} />
+        <Route path="/smurf-form" render={props => <SmurfForm addSmurf={this.addSmurf} />} />
       </div>
     );
   }
